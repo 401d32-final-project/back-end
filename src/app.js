99ac65a8840de89917d44aaffae856ee726e577d
Matthew_Heyert <heyerthb@gmail.com>
2019-12-16 08:40:01 -0800

    basic setup. test route working on port 3000.

---
+++ src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require ('express');
+const cors = require('cors');
+const morgan = require ('morgan');
+
+
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (request, response) => {
+  response.send('you have hit the slash route');
+});
+
+
+module.exports = {
+  server: app,
+  start: (PORT) => {
+    app.listen(PORT, () => {
+      console.log(`Server up on port:: ${PORT}`);
+    });
+  },
+};
