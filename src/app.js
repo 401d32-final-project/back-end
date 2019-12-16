9a966cb0a83cabd5fb898fb4a183b1a0948385c6
Matthew_Heyert <heyerthb@gmail.com>
2019-12-16 11:17:13 -0800

    team push

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 const express = require ('express');
 const cors = require('cors');
 const morgan = require ('morgan');
+const fetchNewsFeed = require ('./routes/newsapi.js');
 
 
 const app = express();
@@ -11,6 +12,10 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+// connects to our news router module
+app.use(fetchNewsFeed);
 
 
 app.get('/', (request, response) => {
